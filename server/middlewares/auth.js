fa24a20012199a3b3533e6173ad94174b3bde334
allenlin90 <ht016746@gmail.com>
2021-09-08 08:12:12 +0700

    update auth middleware and user route, update README.md note

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,19 +1,16 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+// simply check if the token is valid as signed by token secret
 const auth = async function (req, res, next) {
+    const token = req.header('Authorization').split(' ')[1]; // bearer <token>
+    if (!token) return res.status(401).send('unauthorized');
     try {
-        const token = req.header('Authorization').split(' ')[1];
-        const { type, userId, } = jwt.verify(token, process.env.JWT_SECRET);
-
-
-
+        const { userId, } = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = userId;
         next();
     } catch (error) {
-        res.status(401).send({
-            resCode: 401,
-            error: 'Please authenticate',
-            errorMessage: error
-        });
+        res.status(400).send('invalid token');
     }
 }
 
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bycrypt = require('bcrypt');
 const router = new express.Router();
 require('dotenv').config(); // fetch values from env
 
@@ -8,6 +9,17 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 // create a user
+/*
+    1. validate inputs
+    2. check if the user exists by checking both email and OAuth IDs
+    3. store user password
+        3.1 hash password by bcrypt
+        3.2 create random one and hash by bcrypt if user uses OAuth
+    4. create user in database
+    5. validate user information (choose on of the followings)
+        5.1 send user verification email to validate email
+        5.2 send user sms OTP to validate phone number
+*/
 router.post('/', async (req, res) => {
     try {
         const { firstname, lastname, username, password, email, phone, } = req.body;
@@ -17,6 +29,17 @@ router.post('/', async (req, res) => {
     res.send(req.body);
 });
 
+/*
+    1. validate inputs
+    2. check if user exists
+    3. validate user login
+        3.1 verify password by bcrypt
+        3.2 check if user is authorized by OAuth
+    4. generate, sign, and return jwt token to user
+        4.1 HTTP header
+        4.2 HTTP server-side generated cookie
+        4.3 response body
+*/
 router.post('/login', async (req, res) => { });
 
 module.exports = router;
